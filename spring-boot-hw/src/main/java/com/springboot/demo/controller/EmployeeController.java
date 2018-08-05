032a17ff15a6c0c980dff95fd4dd02b6b7e8f717
frieda0503 <ivyvovo@gmail.com>
2018-08-05 20:55:59 +0800

    Update EmployeeController.java

--- spring-boot-hw/src/main/java/com/springboot/demo/controller/EmployeeController.java
+++ spring-boot-hw/src/main/java/com/springboot/demo/controller/EmployeeController.java
@@ -1,24 +1,15 @@
 package com.springboot.demo.controller;
 
+import com.springboot.demo.model.Employee;
+import com.springboot.demo.service.DepartmentService;
+import com.springboot.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.springboot.demo.model.Employee;
-import com.springboot.demo.service.DepartmentService;
-import com.springboot.demo.service.EmployeeService;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -32,32 +23,36 @@ public class EmployeeController {
 
 	// get all data
 	@ResponseStatus(HttpStatus.OK)
-	@GetMapping(value = "/get/employees", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public @ResponseBody Page<Employee> getAll(
-			@RequestParam(required = false) String employeeName,
-			@RequestParam(required = false) Integer employeeId,
-			@RequestParam(required = false) Integer age,
-			@RequestParam(required = false) String departmentName) {
-		return employeeService.getEmployeeData(employeeName, employeeId, age,
-				departmentName);
+	@GetMapping(value = "/employees/all")
+	@ResponseBody
+	public List<Employee> getAll() {
+		return employeeService.getAll();
+	}
 
-		// String employeeName, int employeeId,int age,String departmentName
+	// get all data
+	@ResponseStatus(HttpStatus.OK)
+	@GetMapping(value = "/employees")
+	@ResponseBody
+	public Page<Employee> getAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "size", defaultValue = "5") Integer size) {
+		return employeeService.getAll(page, size);
 	}
 
 	// add data
-	@PostMapping("/add/employee")
+	@PostMapping("/employee")
 	public void addEmployee(@RequestBody Employee employee) {
 		employeeService.addEmployee(employee);
 	}
 
 	// update data
-	@PutMapping("/update/employee")
+	@PutMapping("/employee")
 	public void updateEmployee(@RequestBody Employee employee) {
 		employeeService.updateEmployee(employee);
 	}
 
 	// delete data
-	@DeleteMapping("/delete/employees/{id}")
+	@DeleteMapping("/employees/{id}")
 	public void deleteEmployee(@PathVariable Integer id) {
 		employeeService.deleteEmployee(id);
 	}
