1361c1ac4934e27d09a214b9cd40094a8fac1779
frieda0503 <ivyvovo@gmail.com>
2018-08-05 20:54:53 +0800

    Update DepartmentRepository.java

--- spring-boot-hw/src/main/java/com/springboot/demo/repository/DepartmentRepository.java
+++ spring-boot-hw/src/main/java/com/springboot/demo/repository/DepartmentRepository.java
@@ -1,32 +1,12 @@
 package com.springboot.demo.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.springboot.demo.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.springboot.demo.model.Department;
-
 @RepositoryRestResource
-public interface DepartmentRepository extends
-		JpaRepository<Department, Integer> {
+public interface DepartmentRepository extends JpaRepository<Department, Integer>,
+		JpaSpecificationExecutor<Department> {
 
-	// GET /books/:id
-	@Override
-	Optional<Department> findById(Integer id);
-
-	// GET /books
-	@Override
-	Page<Department> findAll(Pageable pageable);
-
-	// POST /books and PATCH /books/:id
-	@SuppressWarnings("unchecked")
-	@Override
-	Department save(Department s);
-
-	// DELETE /books/:id
-	@Override
-	void deleteById(Integer id);
 }
