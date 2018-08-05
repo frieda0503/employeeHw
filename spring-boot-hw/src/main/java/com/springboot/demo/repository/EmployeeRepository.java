ddede973fa153d141ccc70e11cb1e67f84c1c94a
frieda0503 <ivyvovo@gmail.com>
2018-08-05 20:55:04 +0800

    Update EmployeeRepository.java

--- spring-boot-hw/src/main/java/com/springboot/demo/repository/EmployeeRepository.java
+++ spring-boot-hw/src/main/java/com/springboot/demo/repository/EmployeeRepository.java
@@ -1,34 +1,12 @@
 package com.springboot.demo.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.springboot.demo.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.springboot.demo.model.Employee;
-
 @RepositoryRestResource
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>,
 		JpaSpecificationExecutor<Employee> {
-
-	// GET /employees/:id
-	@Override
-	Optional<Employee> findById(Integer id);
-
-	// GET /employees
-//	@Override
-//	Page<Employee> findAll(Pageable pageable);
-
-	// POST /employees
-	@SuppressWarnings("unchecked")
-	@Override
-	Employee save(Employee e);
-
-	// DELETE /employees/:id
-	@Override
-	void deleteById(Integer id);
 
 }
